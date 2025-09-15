b0a22484044eafd8bcb8e4f14ebd9b345bfc1062
CoRNeRNoTe <cornernote@gmail.com>
2025-09-16 01:51:01 +0930

    cleanup auto updater

--- lua/booster-generator.lua
+++ lua/booster-generator.lua
@@ -5,67 +5,63 @@
 =======================================================================
 ]]--
 
------------------------------------------------------------------------
--- AutoUpdater - downloads the latest version
------------------------------------------------------------------------
-
 local AutoUpdater = {
     name = "Any MTG Booster Generator",
-    version = "1.6.0",
+    version = "1.6.1",
     versionUrl = "https://raw.githubusercontent.com/cornernote/tabletop_simulator-mtg_booster_generator/refs/heads/main/lua/booster-generator.ver",
     scriptUrl = "https://raw.githubusercontent.com/cornernote/tabletop_simulator-mtg_booster_generator/refs/heads/main/lua/booster-generator.lua",
+
+    isNewerVersion = function(self, remoteVersion)
+        local function split(v)
+            local t = {}
+            for n in v:gmatch("%d+") do
+                table.insert(t, tonumber(n))
+            end
+            return t
+        end
+
+        local r, l = split(remoteVersion), split(self.version)
+        for i = 1, math.max(#r, #l) do
+            local rv, lv = r[i] or 0, l[i] or 0
+            if rv > lv then
+                return true
+            end
+            if rv < lv then
+                return false
+            end
+        end
+        return false
+    end,
+
+    fetchNewScript = function(self, newVersion)
+        WebRequest.get(self.scriptUrl, function(request)
+            if request.response_code ~= 200 then
+                return
+            end
+            if request.text and #request.text > 0 then
+                self.host.setLuaScript(request.text)
+                self.host.reload()
+                print(self.name .. ": Updated to version " .. newVersion)
+            end
+        end)
+    end,
+
+    checkForUpdate = function(self)
+        if not self.host then
+            return
+        end
+        WebRequest.get(self.versionUrl, function(request)
+            if request.response_code ~= 200 then
+                print(self.name .. ": Failed to check version (" .. request.response_code .. ")")
+                return
+            end
+            local remoteVersion = request.text:match("[^\r\n]+") or ""
+            if remoteVersion ~= "" and self:isNewerVersion(remoteVersion) then
+                self:fetchNewScript(remoteVersion)
+            end
+        end)
+    end,
 }
-
-AutoUpdater.isNewerVersion = function(remoteVersion)
-    local function split(v)
-        local t = {}
-        for n in v:gmatch("%d+") do
-            table.insert(t, tonumber(n))
-        end
-        return t
-    end
-
-    local r, l = split(remoteVersion), split(AutoUpdater.version)
-    for i = 1, math.max(#r, #l) do
-        local rv, lv = r[i] or 0, l[i] or 0
-        if rv > lv then
-            return true
-        end
-        if rv < lv then
-            return false
-        end
-    end
-    return false
-end
-
-AutoUpdater.checkForUpdate = function()
-    WebRequest.get(AutoUpdater.versionUrl, function(request)
-        if request.response_code ~= 200 then
-            return
-        end
-        local remoteVersion = request.text:match("[^\r\n]+") or ""
-        if remoteVersion ~= "" and AutoUpdater.isNewerVersion(remoteVersion) then
-            AutoUpdater.fetchNewScript(remoteVersion)
-        end
-    end)
-end
-
-AutoUpdater.fetchNewScript = function(newVersion)
-    WebRequest.get(AutoUpdater.scriptUrl, function(request)
-        if request.response_code ~= 200 then
-            return
-        end
-        if request.text and #request.text > 0 then
-            self.setLuaScript(request.text)
-            self.reload()
-            print(AutoUpdater.name .. ": Updated script to version " .. newVersion)
-        end
-    end)
-end
-
------------------------------------------------------------------------
--- Local configuration and data
------------------------------------------------------------------------
 
 local config = {
     backURL = 'https://steamusercontent-a.akamaihd.net/ugc/1647720103762682461/35EF6E87970E2A5D6581E7D96A99F8A575B7A15F/',
